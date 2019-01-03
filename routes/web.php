90dd52b62d241a532b79b57d496ac74bc6fdb080
Marc <zunly@hotmail.com>
2019-01-03 22:14:12 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,10 @@ Route::get('signup', 'UsersController@signup')->name('signup');
 
 Route::resource('users', 'UsersController');
 
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+
 
 
